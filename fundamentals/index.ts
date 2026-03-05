d1ead1aee3421a4ae05f9bfa59beaf590c4bdada
Tim Garcia <timgarciaa@gmail.com>
2026-03-05 21:30:43 +0800

    Double Char

--- fundamentals/index.ts
+++ fundamentals/index.ts
@@ -113,3 +113,15 @@ export function paperwork(n: number, m :number): number{
   }
   return n * m;
 }
+// ------------
+//Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// Good Luck!
+export function doubleChar(str: string): string{
+  return str.split('').map(c => c + c).join('');
+}
+
+console.log(doubleChar('hello')); // 'hheelllloo'
